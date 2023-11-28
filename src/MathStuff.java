372c668ed1cf5350aae99a9d015b93d826645565
adanglingpointer <dave@imail.cloud>
2023-11-27 21:43:27 -0500

    11-27-23

---
+++ src/MathStuff.java
@@ -0,0 +1,25 @@
+public class MathStuff {
+    public static void main(String[] args) {
+        System.out.println(1 * 2 + 3 - 4); // 1
+
+        System.out.println(7 / 4); // 1
+        // integer as default type
+
+        System.out.println(7f / 4); // 1.75
+        // casting a float
+
+        System.out.println((double)7 / 4); // 1.75
+        // casting a double
+
+        System.out.println(7 % 4); // 3
+        // modulo, returns the remainder
+
+        int x = 1;
+        System.out.println(x++); // 1
+        System.out.println(x); // 2
+
+        int y = 1;
+        System.out.println(++y); // 2
+        System.out.println(y); // 2
+    }
+}
