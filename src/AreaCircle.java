9d822c6b8718859dd983cf3a9bbc43498ac48c14
adanglingpointer <dave@imail.cloud>
2023-12-27 20:55:47 -0500

    calculate centripetal acceleration

--- src/AreaCircle.java
+++ src/AreaCircle.java
@@ -1,6 +1,8 @@
 public class AreaCircle {
     public static void main(String[] args) {
-        System.out.println(calcAreaOfCircle(3)); //28.274333882308138
+        System.out.println(calcAreaOfCircle(3)); // 28.274333882308138
+        System.out.println(calcPathVelocity(0.8, 3)); // 1.6755160819145563
+        System.out.println(calcCentripetalAccel(1.67, .8)); // 3.486125
     }
 
     /**
@@ -11,5 +13,14 @@ public class AreaCircle {
     public static double calcAreaOfCircle(double radius) {
         // A = PI * r^2
         return Math.PI * Math.pow(radius, 2);
+    }
+
+    public static double calcPathVelocity(double radius, double period) {
+        double circumference = 2 * Math.PI * radius;
+        return circumference / period;
+    }
+
+    public static double calcCentripetalAccel(double pathVel, double radius) {
+        return Math.pow(pathVel, 2) / radius;
     }
 }
